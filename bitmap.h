aa48be5f114cb50dd1281c0c5036527b08f72c60
fenice sun <fenicesun@FENICE-3.local>
2016-09-07 13:40:59 +0800

    add comment

--- bitmap.h
+++ bitmap.h
@@ -8,18 +8,44 @@ typedef struct {
     int size;
 } bitmap;
 
+/*
+ * init bitmap
+ * return a pointer to bitmap whcih has been allocated in heap
+ *
+ */
 extern bitmap*
 bitmap_init(int size, int start);
 
+
+/*
+ * set the position index value of bitmap
+ *
+ *
+ */
 extern void
 bitmap_set(bitmap *_bitmap, int index);
 
+/*
+ * take the value of position index in bitmap
+ *
+ *
+ */
 extern int
 bitmap_get(bitmap *_bitmap, int index);
 
+
+/*
+ * get the value of position index
+ *
+ *
+ */
 extern int
 bitmap_data(bitmap *_bitmap, int index);
 
+/*
+ * free the memory of bitmap
+ *
+ */
 extern void
 bitmap_free(bitmap *_bitmap);
 
